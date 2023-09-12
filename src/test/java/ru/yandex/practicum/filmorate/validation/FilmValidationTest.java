81d19b6e13b483b95c7165a2777c4adf8a53536f
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-09-13 02:04:23 +0300

    Сдача на проверку практического задания спринта 9

---
+++ src/test/java/ru/yandex/practicum/filmorate/validation/FilmValidationTest.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilmValidationTest {
+    private Film film;
+    private Executable executable;
+
+    @BeforeEach
+    public void beforeEach() {
+        class myExecutable implements Executable {
+
+            @Override
+            public void execute() {
+                FilmValidation fv = new FilmValidation();
+                fv.validate(film);
+            }
+        }
+        executable = new myExecutable();
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenNameIsBlank() {
+        film = new Film(" ","Описание фильма", LocalDate.of(2001, 5, 12),
+                150);
+        film.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У фильма с id=" + film.getId() + " не заполнено название. Название фильма " +
+                "должно быть обязательно!", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenDescriptionIsMoreThen200Symbols() {
+        film = new Film("Фильм","Прям, ну, очень длинное описание фильма: " +
+                "лплппороророророшлопоропрплрллвджаплвдаплщхуклпрхщуопхщшукорщшхоршопгуорпркацпукреке-к8пе" +
+                "шорпшеоркеопршщкеорлещзорещзорзщеорзщеореорщоезорузшлпзукщлпукшлпщзплщкопщкопщкоупококщполкзщ",
+                LocalDate.of(2001, 5, 12), 150);
+        film.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У фильма с id=" + film.getId() + " слишком длинное описание. " +
+                "Максимальная длина описания - 200 символов.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenReleaseDateIsBefore28December1895() {
+        film = new Film("Фильм","Описание фильма", LocalDate.of(1700, 5, 25),
+                150);
+        film.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У фильма с id=" + film.getId() + " некорректно указана дата релиза. " +
+                "Дата релиза не может быть раньше чем 1895-12-28.", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenDurationIsNegative() {
+        film = new Film("Фильм","Описание фильма", LocalDate.of(1995, 7, 18),
+                -150);
+        film.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У фильма с id=" + film.getId() + " некорректно указана продолжительность. " +
+                "Продолжительность фильма должна быть больше нуля!", exception.getMessage());
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
@@ -0,0 +1,81 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserValidationTest {
+    private User user;
+    private Executable executable;
+
+    @BeforeEach
+    public void beforeEach() {
+        class myExecutable implements Executable {
+
+            @Override
+            public void execute() {
+                UserValidation uv = new UserValidation();
+                uv.validate(user);
+            }
+        }
+        executable = new myExecutable();
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenEmailIsBlank() {
+        user = new User(" ", "Esperanto","Миша",
+                LocalDate.of(1992,11,15));
+        user.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У пользователя с id=" + user.getId() + " не заполнен или отсутствует e-mail адрес.",
+                exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenEmailDoesNotContain_Email_Symbol() {
+        user = new User("chizh909mail.ru", "Esperanto","Миша",
+                LocalDate.of(1992,11,15));
+        user.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У пользователя с id=" + user.getId() + " не заполнен или отсутствует e-mail адрес.",
+                exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenLoginIsBlank() {
+        user = new User("chizh909@mail.ru", " ","Миша",
+                LocalDate.of(1992,11,15));
+        user.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У пользователя с id=" + user.getId() + " не заполнен или некорректно указан логин.",
+                exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenLoginContainsSpaces() {
+        user = new User("chizh909@mail.ru", " Esperanto ","Миша",
+                LocalDate.of(1992,11,15));
+        user.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У пользователя с id=" + user.getId() + " не заполнен или некорректно указан логин.",
+                exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowValidateExceptionWhenBirthdayIsAfterCurrentDay() {
+        user = new User("chizh909@mail.ru", "Esperanto","Миша",
+                LocalDate.of(2333,11,15));
+        user.setId(1);
+        final ValidationException exception = assertThrows(ValidationException.class, executable);
+        assertEquals("У пользователя с id=" + user.getId() + " некорректно указана дата рождения.",
+                exception.getMessage());
+    }
+
+}
