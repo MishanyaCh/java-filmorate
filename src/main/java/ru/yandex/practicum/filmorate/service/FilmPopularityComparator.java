7065cb4cea0903f117889deb3218e2bfbc9d94b0
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-10-07 19:52:59 +0300

    style: поправил имена локальных переменных

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmPopularityComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmPopularityComparator.java
@@ -8,8 +8,8 @@ public class FilmPopularityComparator implements Comparator<Film> {
 
     @Override
     public int compare(Film film1, Film film2) {
-        int film1_QuantityLikes = film1.getLikes().size();
-        int film2_QuantityLikes = film2.getLikes().size();
-        return film1_QuantityLikes - film2_QuantityLikes;
+        int quantityLikesByFilm1 = film1.getLikes().size();
+        int quantityLikesByFilm2 = film2.getLikes().size();
+        return quantityLikesByFilm1 - quantityLikesByFilm2;
     }
 }
