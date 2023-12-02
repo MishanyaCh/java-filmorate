a525f580d7007ed196a7db49dac96726704c9938
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-12-02 20:23:17 +0300

    feat: добавил логирование в методе получения жанра по id

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDBStorage.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -7,6 +9,7 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.util.List;
 
 public class GenreDBStorage {
+    private static final Logger log = LoggerFactory.getLogger(GenreDBStorage.class);
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -23,6 +26,7 @@ public class GenreDBStorage {
         String qslQuery = "SELECT * FROM genre WHERE id = ?";
         List<Genre> genres = jdbcTemplate.query(qslQuery, new GenreRowMapper(), id);
         if (genres.isEmpty()) {
+            log.debug("Жанр с id={} не найден в базе данных.", id);
             return null;
         }
         return genres.get(0);
