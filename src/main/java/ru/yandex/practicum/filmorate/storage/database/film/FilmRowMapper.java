300e42e625bee8709c6233ab2ce8e351d97e07cf
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2023-12-13 05:59:52 +0300

    fix: поправил ошибку в методе mapRow для класса FilmRowMapper

--- src/main/java/ru/yandex/practicum/filmorate/storage/database/film/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/database/film/FilmRowMapper.java
@@ -12,14 +12,20 @@ public class FilmRowMapper implements RowMapper<Film> {
 
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
-        int id = rs.getInt("id");
-        String name = rs.getString("name");
-        String description = rs.getString("description");
-        LocalDate releaseDate = rs.getDate("releaseDate").toLocalDate();
-        int duration = rs.getInt("duration");
-
-        int mpaId = rs.getInt("ratingMPA_id");
-        String mpaName = rs.getString("name");
+        // получаем значение из колонки "id" таблицы films
+        int id = rs.getInt(1);
+        // получаем значение из колонки "name" таблицы films
+        String name = rs.getString(2);
+        // получаем значение из колонки "description" таблицы films
+        String description = rs.getString(3);
+        // получаем значение из колонки "releaseDate" таблицы films
+        LocalDate releaseDate = rs.getDate(4).toLocalDate();
+        // получаем значение из колонки "duration" таблицы films
+        int duration = rs.getInt(5);
+        // получаем значение из колонки "ratingMPA_id" таблицы films
+        int mpaId = rs.getInt(6);
+        // получаем значение из колонки "name" таблицы rating_MPA
+        String mpaName = rs.getString(7);
         RatingMPA mpa = new RatingMPA(mpaId, mpaName);
 
         return new Film(id, name, description, releaseDate, duration, mpa);
